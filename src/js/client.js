998a133e880d9faf64aa4b07c5f99932db36f460
Laurent Falda <l.falda@groupe-scala.com>
2016-04-01 17:55:34 +0200

    react start

---
+++ src/js/client.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Layout from './components/Layout';
+
+const app = window.document.getElementById('app');
+ReactDOM.render(<Layout num="3" />, app);
---
+++ src/js/components/Layout.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+module.exports = ({num}) => {
+  return <h1>Title{num}</h1>;
+};
